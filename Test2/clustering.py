c9e6f9463a51fc0b4fb197b56995d1f69d378cd9
Hsiao <10561111@gm.nfu.edu.tw>
2017-05-31 23:53:18 +0800

    0531

---
+++ Test2/clustering.py
@@ -0,0 +1,140 @@
+#coding:utf-8
+
+# clusters for example 北日德蘭大區
+import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets.samples_generator import make_blobs
+
+# Generate sample data
+centers = [[1, 1], [-1, -1], [1, -1]]
+X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
+print X
+
+# http://archive.ics.uci.edu/ml/
+# 北日德蘭大區 https://goo.gl/maps/bMpesdQKyGR2
+# read data
+# with open(r'3D_spatial_network.txt', "r") as ins:
+#     X2 = []
+#     for line in ins:
+#         n = 0
+#         for i in line.split(",")[1:3]:
+#             if n == 1:
+#                 a=float(X3),float(i)
+#                 X2.append(a)
+#             X3=i
+#             n+=1
+# # convert to numpy array
+# X = np.array(X2)
+
+# Compute clustering with MeanShift
+# The following bandwidth can be automatically detected using
+bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=50)
+
+ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+ms.fit(X)
+
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
+
+labels_unique = np.unique(labels)
+n_clusters_ = len(labels_unique)
+
+print("number of estimated clusters : %d" % n_clusters_)
+
+# Plot result
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+plt.figure(1)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    my_members = labels == k
+    cluster_center = cluster_centers[k]
+    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=14)
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+
+
+#
+# # clustering picture
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.cluster import KMeans
+# from sklearn.metrics import pairwise_distances_argmin
+# from sklearn.utils import shuffle
+# from time import time
+#
+# n_colors = 32
+#
+# # load image
+# banana = plt.imread(r"banana.jpg")
+#
+# # Convert to floats instead of the default 8 bits integer coding. Dividing by
+# # 255 is important so that plt.imshow behaves works well on float data (need to
+# # be in the range [0-1])
+# banana = np.array(banana, dtype=np.float64) / 255
+#
+# # Load Image and transform to a 2D numpy array.
+# w, h, d = original_shape = tuple(banana.shape)
+# assert d == 3
+# image_array = np.reshape(banana, (w * h, d))
+#
+# print("Fitting model on a small sub-sample of the data")
+# t0 = time()
+# image_array_sample = shuffle(image_array, random_state=0)[:2000]
+# print("done in %0.3fs." % (time() - t0))
+#
+# # Get labels for all points
+# print("Predicting color indices on the full image (k-means)")
+# t0 = time()
+# kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+# labels = kmeans.predict(image_array)
+# print("done in %0.3fs." % (time() - t0))
+#
+#
+# codebook_random = shuffle(image_array, random_state=0)[:n_colors + 1]
+# print("Predicting color indices on the full image (random)")
+# t0 = time()
+# labels_random = pairwise_distances_argmin(codebook_random,
+#                                           image_array,
+#                                           axis=0)
+# print("done in %0.3fs." % (time() - t0))
+#
+#
+# def recreate_image(codebook, labels, w, h):
+#     """Recreate the (compressed) image from the code book & labels"""
+#     d = codebook.shape[1]
+#     image = np.zeros((w, h, d))
+#     label_idx = 0
+#     for i in range(w):
+#         for j in range(h):
+#             image[i][j] = codebook[labels[label_idx]]
+#             label_idx += 1
+#     return image
+#
+# # Display all results, alongside original image
+# plt.figure(1)
+# plt.clf()
+# ax = plt.axes([0, 0, 1, 1])
+# plt.axis('off')
+# plt.title('Original image (96,615 colors)')
+# plt.imshow(banana)
+#
+# plt.figure(2)
+# plt.clf()
+# ax = plt.axes([0, 0, 1, 1])
+# plt.axis('off')
+# plt.title('Quantized image ('+str(n_colors)+' colors, K-Means)')
+# plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+#
+# plt.figure(3)
+# plt.clf()
+# ax = plt.axes([0, 0, 1, 1])
+# plt.axis('off')
+# plt.title('Quantized image ('+str(n_colors)+' colors, Random)')
+# plt.imshow(recreate_image(codebook_random, labels_random, w, h))
+# plt.show()
